06c506d130f592c5f411b5afee43074a34de3d6a
Bijaya Shrestha <bijayashrestha219@gmail.com>
2023-11-18 12:00:31 +1100

    chore(vite.config.ts): disable public dir copy into output folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
   build: {
+    // Disable copy behavior of all files from the public directory to the output folder.    
+    copyPublicDir: false,
+    
+    // Vite library mode configuration
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
       formats: ['es']
     }
+
   }
 })
