db39ad4a51ce57bd47a3ecad521d6cc7d046ed07
Bijaya Shrestha <bijayashrestha219@gmail.com>
2024-06-04 22:43:56 +1000

    feat(card): basic card style

--- lib/components/Card/index.tsx
+++ lib/components/Card/index.tsx
@@ -1,10 +1,11 @@
+import styles from './styles.module.css';
 
 export function Card(){
     return <>
-        <div>
-            <div>Card Title</div>
-            <div>Card Body</div>
-            <div>Card footer</div>
+        <div className={styles.card}>
+            <div className={styles.card_header}>Card Title</div>
+            <div className={styles.card_body}>Card Body</div>
+            <div className={styles.card_footer}>Card footer</div>
         </div>
     </>
 }
