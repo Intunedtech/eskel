48d3a87cb458dd465a3e8ffe48b7dbb74775aec7
Bijaya Shrestha <bijayashrestha219@gmail.com>
2024-06-04 22:35:06 +1000

    feat(Card): simplest card component and story added

---
+++ lib/components/Card/index.tsx
@@ -0,0 +1,10 @@
+
+export function Card(){
+    return <>
+        <div>
+            <div>Card Title</div>
+            <div>Card Body</div>
+            <div>Card footer</div>
+        </div>
+    </>
+}
