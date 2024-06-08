bd47f9ba32adb1128cbcfc0f217cb31cb6e170cd
Bijaya Shrestha <bijayashrestha219@gmail.com>
2024-06-08 22:17:25 +1000

    feat(vitest): vitest and related packages added as devDependencies

---
+++ src/__tests__/index.test.tsx
@@ -0,0 +1,8 @@
+import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import App from '../App'
+ 
+test('Page', () => {
+  render(<App />)
+  expect(screen.getByRole('heading', { level: 1, name: 'Eskel Demo' })).toBeDefined()
+})
