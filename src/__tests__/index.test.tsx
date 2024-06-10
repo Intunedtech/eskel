0c60cc40261d006c8a6d365d8ae866cb353133d5
Bijaya Shrestha <bijayashrestha219@gmail.com>
2024-06-10 18:36:00 +1000

    fix(index.test.tsx): correct dummy test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,8 +1,5 @@
 import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
-import App from '../App'
  
-test('Page', () => {
-  render(<App />)
-  expect(screen.getByRole('heading', { level: 1, name: 'Eskel Demo' })).toBeDefined()
+test('Vitest HelloWorld', () => {
+  expect(1).toBe(1);
 })
