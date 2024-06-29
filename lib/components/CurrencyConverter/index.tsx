f93c23432de2844e91c767ff199bfa25c50cd106
Bijaya Shrestha <bijayashrestha219@gmail.com>
2024-06-29 19:33:16 +1000

    mod(CurrencyConverter): handled update using function rather than useEffect

--- lib/components/CurrencyConverter/index.tsx
+++ lib/components/CurrencyConverter/index.tsx
@@ -1,18 +1,56 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, SyntheticEvent } from 'react';
 import styles from './styles.module.css';
 
 interface ExchangeRates {
     [key: string]: number;
 }
-
+ 
 const CurrencyConverter = () => {
     const [sendInputValue, setSendInputValue] = useState("");
     const [receiveInputValue, setReceiveInputValue] = useState("");
     const [exchangeRate, setExchangeRate] = useState<Number>(1);
-    const [countrySelectVal, setCountrySelectVal] = useState("");
+    const [targetCurrency, setTargetCurrency] = useState("");
     const [countryNames, setCountryNames] = useState<string[]>([]);
-    const prevExchangeRateRef = useRef<number>(1);
-    const [isUpdating,setIsUpdating] = useState<boolean>(false);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement|HTMLSelectElement>) => {
+        const { name, value } = event.target;
+        switch (name){
+            case 'send':
+                console.log("handling send");
+                setSendInputValue(value);
+                setReceiveInputValue((Number(value) * Number(exchangeRate)).toFixed(2) )
+                break;
+            case 'recieve':
+                console.log("handling recieve");
+                setReceiveInputValue(value);
+                setSendInputValue((Number(value) / Number(exchangeRate)).toFixed(2) )
+                break;
+            case 'targetCurrency':
+                console.log("handling country");
+                setTargetCurrency(value);
+
+                fetch(`https://open.er-api.com/v6/latest/USD`)
+                .then((res) => res.json())
+                .then((data) => {
+                    const rates: ExchangeRates = data.rates;
+                    // console.log("Rates: ", rates);
+                    if (rates[value]) {
+                        console.log(`Currency data for ${value}: `, rates[value]);
+                        setExchangeRate(rates[value]);
+                        setReceiveInputValue((Number(sendInputValue) * rates[value]).toFixed(2) )
+                    }
+                })
+                .catch((error) => console.error('Error fetching exchange rate:', error));
+                
+                
+                break;
+            default:
+                // do nothing
+                break;
+        }
+        console.log(name,value);
+    }
+    
     // Fetch the list of available currencies
     useEffect(() => {
         
@@ -23,43 +61,11 @@ const CurrencyConverter = () => {
                 setCountryNames(currencyCodes);
                 
                 if (currencyCodes.length > 0) {
-                    setCountrySelectVal(currencyCodes[0]);
+                    setTargetCurrency(currencyCodes[0]);
                 }
             })
             .catch((error) => console.error('Error fetching currency codes:', error));
     }, []);
-
-    // Fetch the exchange rate whenever the selected country changes
-    useEffect(() => {
-        if (countrySelectVal) {
-            fetch(`https://open.er-api.com/v6/latest/USD`)
-                .then((res) => res.json())
-                .then((data) => {
-                    const rates: ExchangeRates = data.rates;
-                    console.log("Rates: ", rates);
-                    if (rates[countrySelectVal]) {
-                        setExchangeRate(rates[countrySelectVal]);
-                        console.log(`Currency data for ${countrySelectVal}: `, rates[countrySelectVal]);
-                    }
-                })
-                .catch((error) => console.error('Error fetching exchange rate:', error));
-        }
-    }, [countrySelectVal]);
-
-    // Update receiveInputValue when sendInputValue or exchangeRate changes
-    useEffect(() => {
-        
-            if(!isUpdating){
-                setIsUpdating(true);
-                const receiveCalculation = (Number(sendInputValue) * Number(exchangeRate)).toFixed(2);
-                setReceiveInputValue(receiveCalculation.toString());
-            }
-            
-            return () => {
-                setIsUpdating(false);
-            }
-       
-    }, [receiveInputValue, sendInputValue, exchangeRate]);
 
   return (
     <>
@@ -68,9 +74,10 @@ const CurrencyConverter = () => {
                 <label>
                     Send:
                     <input
+                    name='send'
                     type="number"
                     value={sendInputValue}
-                    onChange={(e) => setSendInputValue(e.target.value)}
+                    onChange={(e) => handleChange(e)}
                     />
                 </label>
             </div>
@@ -78,12 +85,13 @@ const CurrencyConverter = () => {
                 <label>
                     Receive:
                     <input
+                    name='recieve'
                     type="number"
                     value={receiveInputValue}
-                    onChange={(e) => setReceiveInputValue(e.target.value)}
+                    onChange={(e) => handleChange(e)}
                     />
                 </label>
-                <select name="country" value={countrySelectVal} onChange={(e) => setCountrySelectVal(e.target.value)}>
+                <select name="targetCurrency" value={targetCurrency} onChange={(e) => handleChange(e)}>
                     {
                         countryNames.map(countryName => <option key={countryName}>{countryName}</option>)
                     }
