d1a9b77eda647ffa84200ada4fa04202af4a9a6b
Bijaya Shrestha <bijayashrestha219@gmail.com>
2024-07-01 18:58:34 +1000

    mod(CurrencyConverter): Added another switch case 'sendCurrency' to to update currency based on send currency code.

--- lib/components/CurrencyConverter/index.tsx
+++ lib/components/CurrencyConverter/index.tsx
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react';
 import styles from './styles.module.css';
-import { send } from 'process';
 
 interface ExchangeRates {
     [key: string]: number;
 }
- 
+
 const CurrencyConverter = () => {
     const [sendInputValue, setSendInputValue] = useState("");
     const [receiveInputValue, setReceiveInputValue] = useState("");
@@ -14,105 +13,105 @@ const CurrencyConverter = () => {
     const [targetCurrency, setTargetCurrency] = useState("NPR");
     const [countryNames, setCountryNames] = useState<string[]>([]);
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement|HTMLSelectElement>) => {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
         const { name, value } = event.target;
-        
-        switch (name){
+
+        switch (name) {
             case 'send':
                 console.log("handling send");
                 setSendInputValue(value);
-                setReceiveInputValue((Number(value) * Number(exchangeRate)).toFixed(2) )
+                setReceiveInputValue((Number(value) * Number(exchangeRate)).toFixed(2))
                 break;
             case 'recieve':
                 console.log("handling recieve");
                 setReceiveInputValue(value);
-                setSendInputValue((Number(value) / Number(exchangeRate)).toFixed(2) )
+                setSendInputValue((Number(value) / Number(exchangeRate)).toFixed(2))
                 break;
             case 'targetCurrency':
                 setTargetCurrency(value);
                 fetch(`https://open.er-api.com/v6/latest/${sendCurrency}`)
-                .then((res) => res.json())
-                .then((data) => {
-                    const rates: ExchangeRates = data.rates;
-                    if (rates[value]) {
-                        setExchangeRate(rates[value]);
-                        setReceiveInputValue((Number(sendInputValue) * rates[value]).toFixed(2) )
-                    }
-                })
-                .catch((error) => console.error('Error fetching exchange rate:', error));
-                
-                
+                    .then((res) => res.json())
+                    .then((data) => {
+                        const rates: ExchangeRates = data.rates;
+                        if (rates[value]) {
+
+                            setExchangeRate(rates[value]);
+                            setReceiveInputValue((Number(sendInputValue) * rates[value]).toFixed(2))
+                        }
+                    })
+                    .catch((error) => console.error('Error fetching exchange rate:', error));
                 break;
             case 'sendCurrency':
-                console.log("Reached sendCurrency");
-
-                // console log - send value
-                console.log("Send value", sendInputValue);
-                // console log - send currency? 3 char.
-                console.log("send currency:",value)
-                // console log - target currency? 3 char.
-                console.log("Target currency:",targetCurrency)
-
-                break;
+                setSendCurrency(value);
+                fetch(`https://open.er-api.com/v6/latest/${value}`)
+                    .then((res) => res.json())
+                    .then((data) => {
+                        const rates: ExchangeRates = data.rates;
+                        if (rates[targetCurrency]) {
+                            setExchangeRate(rates[targetCurrency]);
+                            setReceiveInputValue((Number(sendInputValue) * rates[targetCurrency]).toFixed(2))
+                        }
+                    })
+              break;
             default:
                 // do nothing
                 break;
         }
-        
+
     }
-    
+
     useEffect(() => {
-        
+        //Fetch a list of currency codes and set exchange rate
         fetch(`https://open.er-api.com/v6/latest/${sendCurrency}`)
-        .then((res) => res.json())
-        .then((data) => {
-            const currencyCodes = Object.keys(data.rates);
-            setCountryNames(currencyCodes);
-            setExchangeRate(data.rates[targetCurrency])
-        })
-        .catch((error) => console.error('Error fetching currency codes:', error));
+            .then((res) => res.json())
+            .then((data) => {
+                const currencyCodes = Object.keys(data.rates);
+                setCountryNames(currencyCodes);
+                setExchangeRate(data.rates[targetCurrency])
+            })
+            .catch((error) => console.error('Error fetching currency codes:', error));
     }, []);
 
-  return (
-    <>
-        <form className={styles.form}>
-            <div className={styles.form_input}>
-                <label>
-                    Send:
-                    <input
-                    name='send'
-                    type="number"
-                    value={sendInputValue}
-                    onChange={(e) => handleChange(e)}
-                    />
-                </label>
-                <select name="sendCurrency" value={sendCurrency} onChange={(e) => handleChange(e)}>
-                    {
-                        countryNames.map(countryName => <option key={countryName}>{countryName}</option>)
-                    }
-                </select>
-            </div>
-            <div>
-                <label>
-                    Receive:
-                    <input
-                    name='recieve'
-                    type="number"
-                    value={receiveInputValue}
-                    onChange={(e) => handleChange(e)}
-                    />
-                </label>
-                <select name="targetCurrency" value={targetCurrency} onChange={(e) => handleChange(e)}>
-                    {
-                        countryNames.map(countryName => <option key={countryName}>{countryName}</option>)
-                    }
-                </select>
-            </div>
-            
-        </form>
-        
-    </>
-  )
+    return (
+        <>
+            <form className={styles.form}>
+                <div className={styles.form_input}>
+                    <label>
+                        Send:
+                        <input
+                            name='send'
+                            type="number"
+                            value={sendInputValue}
+                            onChange={(e) => handleChange(e)}
+                        />
+                    </label>
+                    <select name="sendCurrency" value={sendCurrency} onChange={(e) => handleChange(e)}>
+                        {
+                            countryNames.map(countryName => <option key={countryName}>{countryName}</option>)
+                        }
+                    </select>
+                </div>
+                <div>
+                    <label>
+                        Receive:
+                        <input
+                            name='recieve'
+                            type="number"
+                            value={receiveInputValue}
+                            onChange={(e) => handleChange(e)}
+                        />
+                    </label>
+                    <select name="targetCurrency" value={targetCurrency} onChange={(e) => handleChange(e)}>
+                        {
+                            countryNames.map(countryName => <option key={countryName}>{countryName}</option>)
+                        }
+                    </select>
+                </div>
+
+            </form>
+
+        </>
+    )
 }
 
 export default CurrencyConverter
